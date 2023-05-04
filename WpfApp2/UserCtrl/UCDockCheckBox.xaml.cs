68ab67914618afacee6db59367fb95dd28a60c01
hitsuji <kyoukarada@gmail.com>
2023-05-04 23:53:56 +0900

    ユーザーコントロールの依存関係プロパティ

---
+++ WpfApp2/UserCtrl/UCDockCheckBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2.UserCtrl
+{
+    /// <summary>
+    /// UCDockCheckBox.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UCDockCheckBox : UserControl
+    {
+        public UCDockCheckBox()
+        {
+            InitializeComponent();
+        }
+
+
+        // 依存関係プロパティ
+        public string CheckBoxComment
+        {
+            get { return (string)GetValue(CheckBoxCommentProperty); }
+            set { SetValue(CheckBoxCommentProperty, value); }
+        }
+
+        // CheckBoxComment のバッキング ストアとして DependencyProperty を使用すると、アニメーション、スタイリング、バインディングなどが有効になります。
+        public static readonly DependencyProperty CheckBoxCommentProperty =
+            DependencyProperty.Register("CheckBoxComment", typeof(string), typeof(UCDockCheckBox), new PropertyMetadata(string.Empty));
+
+
+
+    }
+}
